6c0e8be512122cb88269079f98529034b6f5c7a1
John Xanthopoulos <johxan@Johns-MBP.local>
2021-12-31 21:54:45 -0500

    changes lifecyles on S3 logs to remove Glacier and only use ONEZONE_IA

--- s3-logs.tf
+++ s3-logs.tf
@@ -3,26 +3,14 @@ resource "aws_s3_bucket" "logs" {
   bucket = "${var.site_name}-site-logs"
   acl    = "log-delivery-write"
 
-  # Testing lifecycle rules
+  #  Lifecycle rules
   lifecycle_rule {
     id      = "logfile cleanup"
     enabled = true
 
-    # prefix = "log/"
-
-    # tags = {
-    #   rule      = "log"
-    #   autoclean = "true"
-    # }
-
     transition {
       days          = 30
       storage_class = "ONEZONE_IA"
-    }
-
-    transition {
-      days          = 60
-      storage_class = "GLACIER"
     }
     expiration {
       days = 90
